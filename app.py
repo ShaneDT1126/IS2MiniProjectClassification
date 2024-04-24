6365a90ee60609acb5813a86ab7f7cb4c33f2c84
Shijuchi <shanedelatorre2412@gmail.com>
2024-04-25 02:35:52 +0800

    small changes

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 import pickle
+
+import streamlit as st
 
 with open('RandomForest_model.pkl', 'rb') as file:
     RClassifier = pickle.load(file)
